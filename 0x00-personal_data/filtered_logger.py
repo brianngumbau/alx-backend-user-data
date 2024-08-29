ae3b949aee9182a8d836d73bf666a2c2c95d4045
brianngumbau <brianngumbau9075@gmail.com>
2024-08-29 09:03:09 +0000

    fixxing pycodestle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -65,11 +65,10 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Returns a connector to the database."""
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME") or "root"
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD") or ""
-    host = os.getenv("PERSONAL_DATA_DB_HOST") or "localhost"
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", 'root')
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", '')
+    host = os.getenv("PERSONAL_DATA_DB_HOST", 'localhost')
     database = os.getenv("PERSONAL_DATA_DB_NAME")
-
 
     conn = mysql.connector.connect(
         user=username,

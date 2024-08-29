3f7a14deb2e6166f811d6ac42af3e99bfa6a1bed
brianngumbau <brianngumbau9075@gmail.com>
2024-08-29 09:10:43 +0000

    encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Hashing password functions
+"""
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+    Args:
+        password (str): password to be hashed
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed

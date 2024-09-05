7b9428f5f6fc830cc2ac4cfa18c50773f07c312e
brianngumbau <brianngumbau9075@gmail.com>
2024-09-05 20:18:01 +0000

    useer id for session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,7 +14,6 @@ class SessionAuth(Auth):
     """Session Authentication class that inherits from Auth"""
     user_id_by_session_id = {}
 
-
     def create_session(self, user_id: str = None) -> str:
         """Creates a Session ID for a user_id."""
 
@@ -22,3 +21,17 @@ class SessionAuth(Auth):
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the user ID for a given session ID."""
+
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """Returns a User instance based on a cookie value."""
+
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        user = User.get(user_id)
+        return user

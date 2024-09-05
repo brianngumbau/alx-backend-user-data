be02f870e223fd6fce0e3b2f59852d9188f532b3
brianngumbau <brianngumbau9075@gmail.com>
2024-09-05 20:36:38 +0000

    before request

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -71,6 +71,7 @@ class Auth:
         return None
 
     def session_cookie(self, request=None) -> str:
+        """Retrieves the session cookie from a request."""
         if request is not None:
             cookie_name = os.getenv('SESSION_NAME')
             return request.cookies.get(cookie_name)
